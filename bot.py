afd70e48284928ba784ac9b5ef266529e17abbbe
grigory-zhovtun <grigory.zhovtun@gmail.com>
2025-03-06 12:19:43 +0500

    publish text to Telegram channel.

---
+++ bot.py
@@ -0,0 +1,31 @@
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from dotenv import load_dotenv
+import os
+
+
+def start_command(update: Update, context: CallbackContext):
+    update.message.reply_text("Привет! Я тестовый бот. Используй /help, чтобы увидеть список команд.")
+
+
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text("Список доступных команд:\n/start - начать\n/help - помощь")
+
+
+def echo_message(update: Update, context: CallbackContext):
+    text_received = update.message.text
+    update.message.reply_text(f"Вы написали: {text_received}")
+
+
+def main():
+    load_dotenv()
+    TOKEN = os.environ.get('TELEGRAM_API_KEY')
+
+    bot = Bot(TOKEN)
+
+    channel_id = "@space_images_learning_bot"
+
+    bot.send_message(chat_id=channel_id, text="Привет! Это тестовое сообщение в канал.")
+
+if __name__ == "__main__":
+    main()
